0898a444229a973a4fe3de3aa39efe19f659e9e4
Vincent LEBEL <vincent.lebel@efrei.net>
2024-12-17 22:09:14 +0100

    Correction des chemins d'accès des images dans le fichier ecommerce.sql

--- public/database/ecommerce.sql
+++ public/database/ecommerce.sql
@@ -56,23 +56,23 @@ CREATE TABLE carousel_images (
 
 -- Données de la table product
 INSERT INTO product (name, description, picture, price) VALUES
-('RTX 4070', 'Carte Graphique RTX 4070', '/assets/images/cg.jpg', 999.99),
-('RX 7900 XTX', 'Carte Graphique RX 7900 XTX', '/assets/images/cg2.jpg', 999.99),
-('RTX 4070 TI', 'Carte Graphique RTX 4070 TI', '/assets/images/cg3.jpg', 999.99),
-('RTX 4080', 'Carte Graphique RTX 4080', '/assets/images/cg4.jpg', 1299.99),
-('RX 7800 XT', 'Carte Graphique RX 7800 XT', '/assets/images/cg5.jpg', 1099.99),
-('RTX 4060', 'Carte Graphique RTX 4060', '/assets/images/cg6.jpg', 699.99),
-('RTX 4070 TI SUPER 16G', 'MSI GeForce RTX 4070 TI SUPER 16G GAMING X SLIM', '/assets/images/cg7.jpg', 1129.95),
-('RTX 4070 TI SUPER EAGLE OC 16G', 'Gigabyte GeForce RTX 4070 Ti SUPER EAGLE OC 16G', '/assets/images/cg8.jpg', 1129.95),
-('RX 7900 XTX Phantom Gaming 24GB', 'ASRock AMD Radeon RX 7900 XTX Phantom Gaming 24GB', '/assets/images/cg9.jpg', 999.95),
-('RX 7900 XT Vapor-X 20GB', 'Sapphire NITRO+ AMD Radeon RX 7900 XT Vapor-X 20GB', '/assets/images/cg10.jpg', 879.95);
+('RTX 4070', 'Carte Graphique RTX 4070', '/images/cg.jpg', 999.99),
+('RX 7900 XTX', 'Carte Graphique RX 7900 XTX', '/images/cg2.jpg', 999.99),
+('RTX 4070 TI', 'Carte Graphique RTX 4070 TI', '/images/cg3.jpg', 999.99),
+('RTX 4080', 'Carte Graphique RTX 4080', '/images/cg4.jpg', 1299.99),
+('RX 7800 XT', 'Carte Graphique RX 7800 XT', '/images/cg5.jpg', 1099.99),
+('RTX 4060', 'Carte Graphique RTX 4060', '/images/cg6.jpg', 699.99),
+('RTX 4070 TI SUPER 16G', 'MSI GeForce RTX 4070 TI SUPER 16G GAMING X SLIM', '/images/cg7.jpg', 1129.95),
+('RTX 4070 TI SUPER EAGLE OC 16G', 'Gigabyte GeForce RTX 4070 Ti SUPER EAGLE OC 16G', '/images/cg8.jpg', 1129.95),
+('RX 7900 XTX Phantom Gaming 24GB', 'ASRock AMD Radeon RX 7900 XTX Phantom Gaming 24GB', '/images/cg9.jpg', 999.95),
+('RX 7900 XT Vapor-X 20GB', 'Sapphire NITRO+ AMD Radeon RX 7900 XT Vapor-X 20GB', '/images/cg10.jpg', 879.95);
 
 -- donnees de la table carousel_images
 
 INSERT INTO carousel_images (image_path) VALUES
-('/images/Promo.jpg'),
-('/images/Promo2.jpg'),
-('/images/Promo3.jpg'),
-('/images/Promo4.jpg'),
-('/images/Promo5.jpg');
+('/assets/images/Promo.jpg'),
+('/assets/images/Promo2.jpg'),
+('/assets/images/Promo3.jpg'),
+('/assets/images/Promo4.jpg'),
+('/assets/images/Promo5.jpg');
 
